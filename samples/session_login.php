3204242cfb2492f71b7eb951b636d2fae8086c8b
Serge <sergius.nicolaus@gmail.com>
2018-07-04 18:40:00 +0300

    Updated to using new TSDocument class

--- samples/session_login.php
+++ samples/session_login.php
@@ -14,40 +14,10 @@ include("stdcheck.php");
 include("documents.php");
 
 /*
-Firstly, declare class with session support.
-All other pages should inherit from TMySessionDoc or classes inherited from it,
-also possible to use TDBSDocument (defined in stdsdocs.php) as parent class, or
-have a TSession object.
-*/
-class TMySessionDoc extends TDocument {
-
-    public $session;
-
-    public function __construct($title, $checkserv = false, $pvt = false, $redir = "")
-    {
-        $this->session = new TSession($pvt, $redir);
-        parent::__construct($title, $checkserv);
-    }
-
-    public function IsLogged()
-    {
-        return intval($this->session->user) > 0;
-    }
-
-    public function KillSession($msg = "")
-    {
-        $this->session->Disconnect();
-        if ($msg != "") echo "<p>$msg</p>\n";
-        unset($this->session);
-    }
-
-}
-
-/*
 Create login form if not logged, or redirect to homepage if success.
 Use admin/password to login. Also set homepage (success redirect) url.
 */
-class LoginDoc extends TMySessionDoc {
+class LoginDoc extends TSDocument {
 
     private $homepage = "session_home.php"; //set right uri here
     private $checker;
